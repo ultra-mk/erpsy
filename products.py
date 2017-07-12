41287ff1eaae9748471a8e06f6ab8433c4325ff2
michaelkunc <mjkunc@gmail.com>
2017-07-12 16:18:22 -0700

    part number format

--- products.py
+++ products.py
@@ -1,4 +1,4 @@
-import random
+import random as rdm
 from string import ascii_uppercase, digits
 
 
@@ -7,10 +7,18 @@ class Product(object):
     def __init__(self):
         pass
 
-    def part_num(self, format=None):
+    def part_num(self, style=None):
         """
-        Returns a part number in a user specifed format.
-        If no format is given, an 8 digit part number will be returned.
+        Returns a part number in a user specifed style.
+        If no style is given, an 8 digit part number will be returned.
+        Example formats: '#' will return a digit 'A' will return a letter. 
+        Any other character will return that character.
         """
-        if not format:
-            return str(random.randint(10000000, 99999999))
+        if not style:
+            return str(rdm.randint(10000000, 99999999))
+        else:
+            return self._part_num_style(style)
+
+    def _part_num_style(self, style):
+        style = list(style)
+        return ''.join([rdm.choice(digits) if i == '#' else rdm.choice(ascii_uppercase) if i == 'A' else i for i in style])
--- test_products.py
+++ test_products.py
@@ -4,13 +4,25 @@ import products
 
 class ProductTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(ProductTest):
-        ProductTest.ins = products.Product()
-
     def test_part_num_no_format(self):
+        part = products.Product()
         re = r'^[0-9]{8}$'
-        self.assertRegex(ProductTest.ins.part_num(), re)
+        self.assertRegex(part.part_num(), re)
+
+    def test_part_num_style_1(self):
+        part = products.Product()
+        re = r'^[0-9]{4}$'
+        self.assertRegex(part.part_num(style='####'), re)
+
+    def test_part_num_style_2(self):
+        part = products.Product()
+        re = r'^[0-9]{7}$'
+        self.assertRegex(part.part_num(style='#######'), re)
+
+    def test_part_num_style_3(self):
+        part = products.Product()
+        re = r'^[A-Z]{2}[0-9]{2}$'
+        self.assertRegex(part.part_num(style='AA##'), re)
 
 
 if __name__ == '__main__':
