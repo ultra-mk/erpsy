7be32a0aa1df81a920fbf1ba204be2d6920e1487
michaelkunc <mjkunc@gmail.com>
2017-07-15 16:58:11 -0700

    start of bom class.

--- parts.py
+++ parts.py
@@ -23,7 +23,7 @@ class Part(object):
         nouns = ('Resistor', 'Potentiometer', 'Capacitor',
                  'Inductor', 'Oscillator', 'Relay', 'Transformer', 'Battery', 'Integrated Circuit',
                  'Display', 'Condenser', 'Reactor', 'Isolator', 'Control Knob', 'PWB', 'Diode',
-                 'Thermistor', 'CMOS', 'Timer', 'Comparator', 'Regulator', 'Aplifier', 'Cerebra', 'Cerebro')
+                 'Thermistor', 'CMOS', 'Timer', 'Comparator', 'Regulator', 'Amplifier', 'Cerebra', 'Cerebro')
 
         adjectives = ('Active', 'Arc', 'DC', 'Fused', 'Passive',
                       'Electromechanical', 'Constant Current', 'MOSFET', 'Incandescent',
@@ -34,6 +34,31 @@ class Part(object):
 
         return '{0} {1}'.format(rdm.choice(adjectives), rdm.choice(nouns))
 
+    def description(self):
+        descriptions = ('FLIP-FLOP, 2 CIRCUITS', 'Logic IC Case Style',
+                        'PDIP', 'No. of Pins: 14', 'Case Style: PDIP', 'Single Transmitter/Receiver',
+                        'RS-422/RS-485', '8-Pin PDIP Tube', 'XOR Gate', '4-Element', '2-IN Bipolar',
+                        '14-Pin PDIP,XOR Gate', '4-Element 2-IN', 'Bipolar 14-Pin', 'PDIP XOR Gate',
+                        'IBUS', 'JIS', 'DC Block Type', 'Electrical Coil Sensor', 'Type 76553', 'Fiber Optic Circuit',
+                        'Constant Input Resistance', 'Constant Output resistance', 'TI', 'Stark Industries Model',
+                        'Reed Richards Design', 'Later Design Type', 'Reference 22320f', 'Von Doom Captive Design',
+                        'McCoy Style Conversion', 'Cho Model', 'Log Counter Implementatoon', 'MK VI', 'MKVII',
+                        'MacTaggert Implementation', 'Pym Particle Reduction', 'Bannertech')
+
+        return '{0} {1}'.format(rdm.choice(descriptions), rdm.choice(descriptions))
+
     def _number_style(self, style):
         style = list(style)
         return ''.join([rdm.choice(digits) if i == '#' else rdm.choice(ascii_uppercase) if i == 'A' else i for i in style])
+
+
+class BOM(Part):
+
+    def __init__(self, number_of_parts=0):
+        self.number_of_parts = number_of_parts
+
+    def number(self, style=None):
+        return super(BOM, self).number(style)
+
+    def components(self):
+        return [super(BOM, self).__init__() for i in range(0, self.number_of_parts)]
--- test_parts.py
+++ test_parts.py
@@ -32,6 +32,38 @@ class PartTest(unittest.TestCase):
         part = parts.Part()
         self.assertTrue(1 < len(part.name()) < 100)
 
+    def test_description(self):
+        part = parts.Part()
+        self.assertEqual(True, isinstance(part.description(), str))
+
+    def test_description_len(self):
+        part = parts.Part()
+        self.assertTrue(1 < len(part.description()) < 100)
+
+
+class BOMTest(unittest.TestCase):
+
+    def test_bom_no_format(self):
+        bom = parts.BOM()
+        re = r'^[0-9]{8}$'
+        self.assertRegex(bom.number(), re)
+
+    def test_bom_components(self):
+        bom = parts.BOM(number_of_parts=1)
+        self.assertEqual(1, len(bom.components()))
+
+    # def test_bom_components_type(self):
+    #     bom = parts.BOM(number_of_parts=1)
+    #     self.assertEqual('Stuff', bom.components())
+
+    def test_bom_components_55(self):
+        bom = parts.BOM(number_of_parts=55)
+        self.assertEqual(55, len(bom.components()))
+
+    # def test_bom_components_type_55(self):
+    #     bom = parts.BOM(number_of_parts=55)
+    #     self.assertEqual('Stuff', bom.components())
+
 
 if __name__ == '__main__':
     unittest.main()
