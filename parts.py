044c025c16b559210629d0a9e239ae4f03151d79
michaelkunc <mjkunc@gmail.com>
2017-07-18 16:24:59 -0700

    refactor to remove classes

--- parts.py
+++ parts.py
@@ -2,70 +2,55 @@ import random as rdm
 from string import ascii_uppercase, digits
 
 
-class Part(object):
-
-    def __init__(self):
-        pass
-
-    def number(self, style=None):
-        if not style:
-            return str(rdm.randint(10000000, 99999999))
-        else:
-            return self._number_style(style)
-
-    def name(self):
-        nouns = ('Resistor', 'Potentiometer', 'Capacitor',
-                 'Inductor', 'Oscillator', 'Relay', 'Transformer', 'Battery', 'Integrated Circuit',
-                 'Display', 'Condenser', 'Reactor', 'Isolator', 'Control Knob', 'PWB', 'Diode',
-                 'Thermistor', 'CMOS', 'Timer', 'Comparator', 'Regulator', 'Amplifier', 'Cerebra', 'Cerebro')
-
-        adjectives = ('Active', 'Arc', 'DC', 'Fused', 'Passive',
-                      'Electromechanical', 'Constant Current', 'MOSFET', 'Incandescent',
-                      'Diode', 'MIS', 'Piezoelectrical', 'Choke', 'Solenoid', 'Selenium',
-                      'Distributed', 'Voltage Regulation', 'Light Emitting', 'Variable Capacitance',
-                      'Carbon Film', 'Metal Film', 'Variable', 'CDS', 'NTC', 'PTC', 'CTR', 'Electrolytic',
-                      'Tantalum', 'Ceramic', 'Multilayer', 'Polystyrene', 'Polypropylene', 'Mica', 'Repulsing')
-
-        return '{0} {1}'.format(rdm.choice(adjectives), rdm.choice(nouns))
-
-    def description(self):
-        descriptions = ('FLIP-FLOP, 2 CIRCUITS', 'Logic IC Case Style',
-                        'PDIP', 'No. of Pins: 14', 'Case Style: PDIP', 'Single Transmitter/Receiver',
-                        'RS-422/RS-485', '8-Pin PDIP Tube', 'XOR Gate', '4-Element', '2-IN Bipolar',
-                        '14-Pin PDIP,XOR Gate', '4-Element 2-IN', 'Bipolar 14-Pin', 'PDIP XOR Gate',
-                        'IBUS', 'JIS', 'DC Block Type', 'Electrical Coil Sensor', 'Type 76553', 'Fiber Optic Circuit',
-                        'Constant Input Resistance', 'Constant Output resistance', 'TI', 'Stark Industries Model',
-                        'Reed Richards Design', 'Later Design Type', 'Reference 22320f', 'Von Doom Captive Design',
-                        'McCoy Style Conversion', 'Cho Model', 'Log Counter Implementatoon', 'MK VI', 'MKVII',
-                        'MacTaggert Implementation', 'Pym Particle Reduction', 'Bannertech')
-
-        return '{0} {1}'.format(rdm.choice(descriptions), rdm.choice(descriptions))
-
-    def uom(self):
-        uoms = ('Each', 'Batch', 'Case', 'Unit')
-        return rdm.choice(uoms)
-
-    def _number_style(self, style):
-        style = list(style)
-        return ''.join([rdm.choice(digits) if i == '#' else rdm.choice(ascii_uppercase) if i == 'A' else i for i in style])
+def number(style=None):
+    if not style:
+        return str(rdm.randint(10000000, 99999999))
+    else:
+        return _number_style(style)
 
 
-class BOM(Part):
+def name():
+    nouns = ('Resistor', 'Potentiometer', 'Capacitor',
+             'Inductor', 'Oscillator', 'Relay', 'Transformer', 'Battery', 'Integrated Circuit',
+             'Display', 'Condenser', 'Reactor', 'Isolator', 'Control Knob', 'PWB', 'Diode',
+             'Thermistor', 'CMOS', 'Timer', 'Comparator', 'Regulator', 'Amplifier', 'Cerebra', 'Cerebro')
 
-    def __init__(self, number_of_parts=0, depth=1):
-        self.number_of_parts = number_of_parts
-        self.depth = depth
+    adjectives = ('Active', 'Arc', 'DC', 'Fused', 'Passive',
+                  'Electromechanical', 'Constant Current', 'MOSFET', 'Incandescent',
+                  'Diode', 'MIS', 'Piezoelectrical', 'Choke', 'Solenoid', 'Selenium',
+                  'Distributed', 'Voltage Regulation', 'Light Emitting', 'Variable Capacitance',
+                  'Carbon Film', 'Metal Film', 'Variable', 'CDS', 'NTC', 'PTC', 'CTR', 'Electrolytic',
+                  'Tantalum', 'Ceramic', 'Multilayer', 'Polystyrene', 'Polypropylene', 'Mica', 'Repulsing')
 
-    def number(self, style=None):
-        return Part.number(self, style)
+    return '{0} {1}'.format(rdm.choice(adjectives), rdm.choice(nouns))
 
-    def components(self, style=None):
-        return [{'PN': Part.number(style), 'Name': Part.name(self),
-                 'Desc': Part.description(self), 'Level': rdm.choice(range(1, self.depth + 1)),
-                 'QTY': rdm.choice(range(1, 9))}
-                for i in range(0, self.number_of_parts)]
 
-    def name(self):
-        nouns = ('Gentech', 'McCoy', 'Test', 'Motor')
-        adjectives = ('Kit', 'Assy', 'Assembly')
-        return "{0} {1}".format(nouns, adjectives)
+def description():
+    descriptions = ('FLIP-FLOP, 2 CIRCUITS', 'Logic IC Case Style',
+                    'PDIP', 'No. of Pins: 14', 'Case Style: PDIP', 'Single Transmitter/Receiver',
+                    'RS-422/RS-485', '8-Pin PDIP Tube', 'XOR Gate', '4-Element', '2-IN Bipolar',
+                    '14-Pin PDIP,XOR Gate', '4-Element 2-IN', 'Bipolar 14-Pin', 'PDIP XOR Gate',
+                    'IBUS', 'JIS', 'DC Block Type', 'Electrical Coil Sensor', 'Type 76553', 'Fiber Optic Circuit',
+                    'Constant Input Resistance', 'Constant Output resistance', 'TI', 'Stark Industries Model',
+                    'Reed Richards Design', 'Later Design Type', 'Reference 22320f', 'Von Doom Captive Design',
+                    'McCoy Style Conversion', 'Cho Model', 'Log Counter Implementatoon', 'MK VI', 'MKVII',
+                    'MacTaggert Implementation', 'Pym Particle Reduction', 'Bannertech')
+
+    return '{0} {1}'.format(rdm.choice(descriptions), rdm.choice(descriptions))
+
+
+def uom():
+    uoms = ('Each', 'Batch', 'Case', 'Unit')
+    return rdm.choice(uoms)
+
+
+def component_list(style=None, number_of_parts=1, levels=1):
+    return [{'PN': number(style), 'Name': name(),
+             'Desc': description(), 'Level': rdm.choice(range(1, levels + 1)),
+             'QTY': rdm.choice(range(1, 9))}
+            for i in range(0, number_of_parts)]
+
+
+def _number_style(style):
+    style = list(style)
+    return ''.join([rdm.choice(digits) if i == '#' else rdm.choice(ascii_uppercase) if i == 'A' else i for i in style])
--- test_parts.py
+++ test_parts.py
@@ -7,92 +7,41 @@ RE = {'default': r'^[0-9]{8}$',
       '3': r'^[A-Z]{2}[0-9]{2}$'}
 
 
-class PartTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(PartTest):
-        PartTest.data = parts.Part()
+class PartsTest(unittest.TestCase):
 
     def test_part_num_default(self):
-        self.assertRegex(PartTest.data.number(), RE['default'])
+        self.assertRegex(parts.number(), RE['default'])
 
     def test_part_num_style_1(self):
-        self.assertRegex(PartTest.data.number(style='####'), RE['1'])
+        self.assertRegex(parts.number(style='####'), RE['1'])
 
     def test_part_num_style_2(self):
-        self.assertRegex(PartTest.data.number(style='#######'), RE['2'])
+        self.assertRegex(parts.number(style='#######'), RE['2'])
 
     def test_part_num_style_3(self):
-        self.assertRegex(PartTest.data.number(style='AA##'), RE['3'])
+        self.assertRegex(parts.number(style='AA##'), RE['3'])
 
-    def test_name(self):
-        self.assertTrue(isinstance(PartTest.data.name(), str))
+    def test_str_types(self):
+        properties = [parts.name(), parts.description(), parts.uom(),
+                      parts.component_list()[0]['Name'],
+                      parts.component_list()[0]['Desc']]
+        self.assertTrue(all(isinstance(x, str) for x in properties))
 
-    def test_name_len(self):
-        self.assertTrue(1 < len(PartTest.data.name()) < 100)
-
-    def test_description(self):
-        self.assertTrue(isinstance(PartTest.data.description(), str))
-
-    def test_description_len(self):
-        self.assertTrue(1 < len(PartTest.data.description()) < 100)
-
-    def test_uom(self):
-        self.assertTrue(isinstance(PartTest.data.uom(), str))
-
-
-class BOMTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(BOMTest):
-        BOMTest.data = parts.BOM()
-
-    def test_bom_num_default(self):
-        self.assertRegex(BOMTest.data.number(), RE['default'])
-
-    def test_bom_num_style1(self):
-        self.assertRegex(BOMTest.data.number(style='####'), RE['1'])
-
-    def test_part_num_style_2(self):
-        self.assertRegex(BOMTest.data.number(style='#######'), RE['2'])
-
-    def test_bom_style_3(self):
-        self.assertRegex(BOMTest.data.number(style='AA##'), RE['3'])
+    def test_int_types(self):
+        properties = [parts.component_list()[0]['Level'],
+                      parts.component_list()[0]['QTY'], ]
+        self.assertTrue(all(isinstance(x, int) for x in properties))
 
     def test_bom_components(self):
-        bom = parts.BOM(number_of_parts=1)
-        self.assertEqual(1, len(bom.components()))
-
-    def test_bom_components_type(self):
-        bom = parts.BOM(number_of_parts=1)
-        self.assertRegex(bom.components()[0]['PN'], RE['default'])
+        self.assertEqual(1, len(parts.component_list()))
 
     def test_bom_components_55(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertEqual(55, len(bom.components()))
+        self.assertEqual(55, len(parts.component_list(number_of_parts=55)))
 
-    def test_bom_components_type_55(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertRegex(bom.components()[-1]['PN'], RE['default'])
+    def test_component_properties(self):
+        properties = ['PN', 'Name', 'Desc', 'Level', 'QTY']
+        self.assertTrue(set(parts.component_list()[0]) == set(properties))
 
-    def test_bom_name(self):
-        self.assertTrue(isinstance(BOMTest.data.name(), str))
-
-    def test_component_name(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertTrue(isinstance(bom.components()[0]['Name'], str))
-
-    def test_component_desc(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertTrue(isinstance(bom.components()[0]['Desc'], str))
-
-    def test_component_depth(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertTrue(isinstance(bom.components()[0]['Level'], int))
-
-    def test_component_QTY(self):
-        bom = parts.BOM(number_of_parts=55)
-        self.assertTrue(isinstance(bom.components()[0]['QTY'], int))
 
 if __name__ == '__main__':
     unittest.main()
