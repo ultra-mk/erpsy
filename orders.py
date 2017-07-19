bc49ab44fc2ac04588d15cf77ca01c2638a05503
michaelkunc <mjkunc@gmail.com>
2017-07-19 12:53:50 -0700

    start of orders.py and test_orders.py

---
+++ orders.py
@@ -0,0 +1,2 @@
+import random as rdm
+from parts import number, _number_style
---
+++ test_orders.py
@@ -0,0 +1,14 @@
+import unittest
+import orders
+
+RE = {'default': r'^[0-9]{8}$'}
+
+
+class OrdersTest(unittest.TestCase):
+
+    def test_order_num_default(self):
+        self.assertRegex(orders.number(), RE['default'])
+
+
+if __name__ == '__main__':
+    unittest.main()
